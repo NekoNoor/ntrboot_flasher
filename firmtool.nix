2968213a7ad51c4a53f0a5fb8d28c41b6623a35f
Mia Winter Schat <nekonoor@proton.me>
2024-05-02 02:10:45 +0200

    feat: add files to compile in nix-shell

---
+++ firmtool.nix
@@ -0,0 +1,23 @@
+{ lib
+, python3Packages
+, fetchFromGitHub
+}:
+
+with python3Packages;
+buildPythonApplication rec {
+  pname = "firmtool";
+  version = "unstable-2020-12-26";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = "TuxSH";
+    repo = "firmtool";
+    rev = "fdc7085c2394d87ce5dbdfecdf51423e1e7b00a1";
+    sha256 = "0pa5w0h3nj53xdm69sfp1834vijknggndqnjp84f346vfrwcryzd";
+  };
+
+  propagatedBuildInputs = [
+    pycryptodome
+  ];
+}
+
---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devkitnix = {
+      url = "github:NekoNoor/devkitnix/devkitarm-20180522";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devkitnix,
+    flake-compat,
+  }: let
+    pkgs = import nixpkgs {system = "x86_64-linux";};
+    devkitARM = devkitnix.packages.x86_64-linux.devkitARM;
+    libc = pkgs.writeText "libc.txt" ''
+      include_dir=${devkitARM}/devkitARM/arm-none-eabi/include
+      sys_include_dir=${devkitARM}/devkitARM/arm-none-eabi/include
+      crt_dir=${devkitARM}/devkitARM/lib
+      msvc_lib_dir=
+      kernel32_lib_dir=
+      gcc_dir=
+    '';
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      buildInputs = [
+        devkitARM
+      ];
+      nativeBuildInputs = [
+        (pkgs.callPackage ./firmtool.nix {})
+      ];
+      shellHook = ''
+        export LIBC=${libc}
+        export DEVKITPRO=${devkitARM}
+        export DEVKITARM=${devkitARM}/devkitARM
+      '';
+    };
+  };
+}
